ddf7c3536509e6c5b07b5689171ac4080991d3b5
asif0927 <6zd6jws@code.edu.az>
2023-06-11 21:05:34 +0400

    cow section completed

--- front/src/pages/User/Home/index.jsx
+++ front/src/pages/User/Home/index.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Drink from "../../../sections/Drink";
+import Cow from "../../../sections/Cow";
 const index = () => {
   return (
     <>
+    <Cow/>
      <Drink/>
     </>
   )
---
+++ front/src/sections/Cow/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import style from "./index.module.css";
+const index = () => {
+  return (
+    <>
+    <section className={style.section}>
+        <div className={style.flex}>
+            <div style={{marginTop:"20%"}}>
+            <h2 className={style.title}>Steak perfection</h2>
+            <h2 className={style.title}>takes time</h2>
+            <p style={{textAlign:"center",marginTop:"30px"}}>
+            <span className={style.span}>I'm a paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit<br></br> Text” or double click me to add your own content and make changes to the font.&nbsp;I'm a<br></br> paragraph. Click here to add your own text and edit me. It’s easy. Just click “Edit Text” or<br></br> double click me to add your own content and make changes to the font.</span>
+            </p>
+            </div>
+        </div>
+     </section>
+    </>
+  )
+}
+
+export default index
